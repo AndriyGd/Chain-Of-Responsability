fc0f4bfcbcce2bad2689bf1af9ad4d641d8107ac
a.gladkyi <a.gladkyi@W-VN-SOB35O-016>
2017-09-04 12:10:18 +0300

    Add cldss - Order

---
+++ ChainFilters/Model/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using PropertyChanged;
+
+namespace ChainFilters.Model
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Order : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => {};
+
+        public string NumberOrder { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Customer { get; set; }
+        public byte OrderStatus { get; set; }
+    }
+}
